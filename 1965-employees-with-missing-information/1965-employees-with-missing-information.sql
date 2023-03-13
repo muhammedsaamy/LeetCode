2c1b6afd616cd9f7e4cada6560258cba4e511c65
Muhammed Samy <112627729+muhammedsaamy@users.noreply.github.com>
2023-03-13 18:02:20 +0200

    Time: 915 ms (8.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select employee_id from Employees 
+where employee_id not in (select employee_id from salaries)
+union all 
+select employee_id from salaries 
+where employee_id not in (select employee_id from Employees)
+
+order by employee_id asc
