b1c74ab59a711a29da6bf2e9aec6048ccb7f6ac3
Muhammed Samy <112627729+muhammedsaamy@users.noreply.github.com>
2023-03-15 23:06:10 +0200

    Time: 1075 ms (17.33%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+select w1.id from weather w1
+left join weather w2 on w1.recordDate = dateadd(day, 1, w2.recordDate)
+where w1.temperature > w2.temperature 
